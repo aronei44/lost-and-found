1e758ded416300c4eb397be152ab389571966270
aronei44 <meliodasmeliodas224@gmail.com>
2025-05-18 17:39:12 +0700

    feat (backend): argument

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,6 +6,22 @@ mod route;
 
 use dotenvy::dotenv;
 use tracing_subscriber::EnvFilter;
+use std::net::SocketAddr;
+
+// Import the Parser derive macro and trait from clap
+use clap::Parser;
+
+/// Server CLI
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long, default_value = "0.0.0.0")]
+    host: String,
+
+    #[clap(long, default_value = "8000")]
+    port: u16,
+}
+
+
 
 #[tokio::main]
 async fn main() {
@@ -15,11 +31,13 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
     let app = app::create_app();
-    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-    let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let host = format!("{}:{}", host, port);
-    tracing::info!("Starting server on {}", host);
+    // Default values
+    let args = Args::parse();
+
+    let addr: SocketAddr = format!("{}:{}", args.host, args.port).parse().expect("Invalid host or port");
+
+    tracing::info!("Starting server on {}", addr);
     // Create a TCP listener on the specified host and port
-    let listener = tokio::net::TcpListener::bind(host).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
